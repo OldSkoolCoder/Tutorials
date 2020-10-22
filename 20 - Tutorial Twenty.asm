02af8b31cc418e359ec71569e3da0070c320382e
OldSkoolCoder <JohnDale@Dalesoft.com>
2020-10-22 17:52:20 +0100

    Tutorial Numbering Corrected.

--- 20 - Tutorial Twenty.asm
+++ 20 - Tutorial Twenty.asm
@@ -1,9 +1,9 @@
 ;*******************************************************************************
-;* Tutorial Nineteen Output to a device using Kernel Jump Vectors              *
+;* Tutorial Twenty-One Input from a device using Kernel Jump Vectors           *
 ;*                                                                             *
 ;* Written By John C. Dale                                                     *
-;* Tutorial #20                                                                *
-;* Date : 24th Aug, 2017                                                       *
+;* Tutorial #21                                                                *
+;* Date : 29th Sep, 2017                                                       *
 ;*                                                                             *
 ;*******************************************************************************
 ;*                                                                             *
@@ -14,6 +14,8 @@
 ;*******************************************************************************
 
 CHROUT  = $FFD2
+CHRIN   = $FFCF
+CHKIN   = $FFC6
 CHKOUT  = $FFC9
 OPEN    = $FFC0
 SETLFS  = $FFBA
@@ -22,59 +24,33 @@ CLRCHN  = $FFCC
 CLOSE   = $FFC3
 
 PRINTSTRING = $AB1E
+BUFFERTEXT  = $0200
 
 *=$9000
-    ;jmp PRINTTODEFAULT
-    ;jmp PRINTTOPRINTER
-    ;jmp PRINTTOTAPE
-    jmp PRINTTODISK
+    ;jmp INPUTFROMDEFAULT
+    ;jmp INPUTFROMTAPE
+    ;jmp INPUTFROMDISK
 
 TESTFILENAME
     TEXT "johntest"
     BRK
 
 TESTFILENAMEDISK
-    TEXT "johntest,seq,write"
+    TEXT "johntest,seq,read"
     BRK
 
 TESTTEXT
-    TEXT "this is to test the output jump vectors"
-    BYTE 15
+    TEXT "                                                        "
     BRK
 
-PRINTTODEFAULT
-    ldx #$00
-    lda #<TESTTEXT
-    ldy #>TESTTEXT
-    jsr PRINTSTRING
+INPUTFROMDEFAULT
+    jsr INPUTSTRING
     rts
 
-PRINTTOPRINTER
-    lda #4      ; Logical File Number
-    tax         ; Device Number (Printer Device #4)
-    ldy #2      ; Secondary Address
-    jsr SETLFS
-    lda #0
-    ldx #255
-    ldy #255
-    jsr SETNAM
-    jsr OPEN
-    ldx #4      ; Logical File Number
-    jsr CHKOUT
-    jsr PRINTTODEFAULT
-    ;ldx #$00
-    ;lda #<TESTTEXT
-    ;ldy #>TESTTEXT
-    ;jsr PRINTSTRING
-    jsr CLRCHN
-    lda #4      ; Logical File Number
-    jsr CLOSE
-    rts
-    
-PRINTTOTAPE
+INPUTFROMTAPE
     lda #1      ; Logical File Number
     tax         ; Device Number (Tape Device #1)
-    ldy #255    ; Secondary Address
+    ldy #0      ; Secondary Address (0 = Loading)
     jsr SETLFS
     lda #8
     ldx #<TESTFILENAME
@@ -82,63 +58,44 @@ PRINTTOTAPE
     jsr SETNAM
     jsr OPEN
     ldx #1      ; Logical File Number
-    jsr CHKOUT
-    jsr PRINTTODEFAULT
-    ;ldx #$00
-    ;lda #<TESTTEXT
-    ;ldy #>TESTTEXT
-    ;jsr PRINTSTRING
+    jsr CHKIN
+    jsr INPUTFROMDEFAULT
     jsr CLRCHN
     lda #1      ; Logical File Number
     jsr CLOSE
     RTS
 
-PRINTTODISK
+INPUTFROMDISK
     lda #8      ; Logical File Number
     tax         ; Device Number (Disk Drive 8)
     ldy #2      ; Secondary Address
     jsr SETLFS
-    lda #18
+    lda #17
     ldx #<TESTFILENAMEDISK
     ldy #>TESTFILENAMEDISK
     jsr SETNAM
     jsr OPEN
-;    bcc NOOPENERRORFOUND
-;    cmp #1      ; To Many Files Open
-;    bne @NEXT
-;    jmp TOOMANYFILESOPENERROR
-;@NEXT
-;    cmp #2      ; File Already Open
-;    bne @NEXT2
-;    jmp FILEALREADYOPENERROR
-;@NEXT2
-;    cmp #4      ; File Not Found
-;    bne @NEXT3
-;    jmp FILENOTFOUNDERROR
-;@NEXT3
-;    cmp #5      ; Device Not Present
-;    bne @NEXT4
-;    jmp DEVICENOTPRESENTERROR
-;@NEXT4
-;    cmp #6      ; File is not an Input File
-;    bne NOOPENERRORFOUND
-;    jmp FILENOTINPUTFILEERROR
-;NOOPENERRORFOUND
     ldx #8      ; Logical File Number
-    jsr CHKOUT
-    bcc NOCHKOUTERRORFOUND
-    cmp #0      ; Routine Terminated By Stop Key
-    cmp #3      ; File Not Open
-    cmp #5      ; Device Not Present
-    cmp #7      ; File is not an Output File
+    jsr CHKIN
 
 NOCHKOUTERRORFOUND
-    jsr PRINTTODEFAULT
-    ;ldx #$00
-    ;lda #<TESTTEXT
-    ;ldy #>TESTTEXT
-    ;jsr PRINTSTRING
+    jsr INPUTFROMDEFAULT
     jsr CLRCHN
     lda #8      ; Logical File Number
     jsr CLOSE
     RTS
+
+INPUTSTRING
+    ldx #0
+@INPUTSTRINGLOOP
+    jsr $e112
+    cmp #13
+    beq @INPUTEND
+    sta TESTTEXT,x
+    inx
+    cpx #56
+    bcc @INPUTSTRINGLOOP
+@INPUTEND
+    RTS
+
+    
